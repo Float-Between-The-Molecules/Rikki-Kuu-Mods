179f2f01ec06dfc9a8f712c08b2dbd4b810ee84a
The Anger <69954363+the-anger@users.noreply.github.com>
2025-05-15 11:27:26 +1000

    Project Setup
    * relocate base mod files from Rikki-Kuu
    
    * default mod assembly name
    
    * discontinue C# and reset
    
    * add framework as submodule
    
    * Update framework
    
    * godot project setup
    
    * base mod prototyping
    
    * move base mod into main game
    
    * add gamepad mod

---
+++ mods/float_gamepad/mod.gd
@@ -0,0 +1,34 @@
+#@icon
+extends RikkiKuuMod
+## Experimental Official Gamepad support
+
+# signals
+# enums
+# constants
+# static variables
+# @export variables
+# remaining regular variables
+# @onready variables
+
+# _static_init()
+# remaining static methods
+# _init()
+# _enter_tree()
+# _ready()
+# _process()
+# _physics_process()
+# remaining virtual methods
+
+#region overridden custom methods
+
+func get_shortname() -> String:
+	return "float_gamepad"
+
+
+func get_longname() -> String:
+	return "Gamepad Support Experiment"
+
+#endregion
+
+# remaining methods
+# subclasses
